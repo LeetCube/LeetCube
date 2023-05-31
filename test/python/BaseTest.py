d75398abf01288b82a84aa047d2ce53f5505fcdc
dataprob <86937926+dataprob@users.noreply.github.com>
2023-05-31 01:03:39 -0700

    format.md added python and JS, formatted python

--- test/python/BaseTest.py
+++ test/python/BaseTest.py
@@ -4,12 +4,17 @@ from typing import Union
 
 
 def default_output_fetcher(case):
-    return case['output']
+    return case["output"]
 
 
 class BaseTest:
-    def __init__(self, number: Union[int, str], input_fetcher, output_fetcher=default_output_fetcher):
-        with open(f'../test_json/test_{number}.json', 'r') as f:
+    def __init__(
+        self,
+        number: Union[int, str],
+        input_fetcher,
+        output_fetcher=default_output_fetcher,
+    ):
+        with open(f"../test_json/test_{number}.json", "r") as f:
             self.data = json.load(f)
             self.input_fetcher = input_fetcher
             self.output_fetcher = output_fetcher
@@ -19,9 +24,9 @@ class BaseTest:
         for case in self.data:
             input = self.input_fetcher(case)
             answer = self.output_fetcher(case)
-            assert solution(input) == answer, \
-                f"Failed case. Should have been {answer}: {input}"
+            assert (
+                solution(input) == answer
+            ), f"Failed case. Should have been {answer}: {input}"
 
         end = timer()
         print(f"Elapsed time: {(end - start) * 1000: .0f} ms")
-
