60bd17e074be52d2fbbbad8389f6b6ee5dda1ed3
PhaseRush <PhaseAnsible@gmail.com>
2023-05-31 20:43:49 -0700

    Add code 2 with a test restructure for arbitrary answer comparison

--- test/python/BaseTest.py
+++ test/python/BaseTest.py
@@ -1,18 +1,47 @@
 import json
 from timeit import default_timer as timer
-from typing import Union
+from typing import Union, List, Optional
+
+from Structure import ListNode
 
 
 def default_output_fetcher(case):
-    return case['output']
+    return case["output"]
+
+
+def build_linked_list(input: List) -> Optional[ListNode]:
+    if len(input) == 0:
+        return None
+    head = ListNode(input[0])
+    curr = head
+    for i in input[1:]:
+        curr.next = ListNode(i)
+        curr = curr.next
+    return head
+
+
+def listnode_to_list(ln: ListNode):
+    acc = []
+    while ln:
+        acc.append(ln.val)
+        ln = ln.next
+
+    return acc
 
 
 class BaseTest:
-    def __init__(self, number: Union[int, str], input_fetcher, output_fetcher=default_output_fetcher):
-        with open(f'../test_json/test_{number}.json', 'r') as f:
+    def __init__(
+        self,
+        number: Union[int, str],
+        input_fetcher,
+        output_fetcher=default_output_fetcher,
+        output_transformer=None,
+    ):
+        with open(f"../test_json/test_{number}.json", "r") as f:
             self.data = json.load(f)
             self.input_fetcher = input_fetcher
             self.output_fetcher = output_fetcher
+            self.output_transformer = output_transformer
 
     def run_tests(self, solution):
         start = timer()
@@ -23,9 +52,11 @@ class BaseTest:
                 actual = solution(*input)
             else:
                 actual = solution(input)
-            assert actual == answer, \
-                f"Failed case. Should have been {answer}, was {actual}\n: {input}"
+            if self.output_transformer is not None:
+                actual = self.output_transformer(actual)
+            assert (
+                actual == answer
+            ), f"Failed case. Should have been {answer}, was {actual}\n: {input}"
 
         end = timer()
         print(f"{len(self.data)} cases ran in{(end - start) * 1000: .0f} ms")
-
