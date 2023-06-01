13d0a8ab36adb87d65efd254c6f08cf3b034c5aa
PhaseRush <PhaseAnsible@gmail.com>
2023-06-01 13:38:37 -0700

    Add Q49 py and generalize BaseTest.py further (#16)
    * Add 2sum for py. Modify base test to support multiple inputs
    
    * Remove redundant import
    
    * Add code 2 with a test restructure for arbitrary answer comparison
    
    * Test 1 lint fix
    
    * Add Q49 py and generalize BaseTest.py further
    
    * Add Q49 py and generalize BaseTest.py further

---
+++ test/python/test_49.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from BaseTest import BaseTest
+from src.python.code_49 import Solution
+
+
+def input_fetcher(case):
+    return case["input"]["strs"]
+
+
+def nested_list_contents_comparator(
+    answer: List[List[str]], actual: List[List[str]]
+) -> bool:
+    if len(answer) != len(actual):
+        return False
+
+    answer.sort()
+    actual.sort()
+
+    for ans, act in zip(answer, actual):
+        if ans.sort() != act.sort():
+            return False
+
+    return True
+
+
+if __name__ == "__main__":
+    test = BaseTest(
+        49, input_fetcher, output_comparator=nested_list_contents_comparator
+    )
+    solution = Solution().groupAnagrams
+    test.run_tests(solution)
