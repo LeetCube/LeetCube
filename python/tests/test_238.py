ee72dc75df2a8e1463ef273ae1971ebbdf6b9e87
PhaseRush <PhaseAnsible@gmail.com>
2023-06-02 09:47:26 -0700

    Add py 238 and 347 (#23)

---
+++ python/tests/test_238.py
@@ -0,0 +1,12 @@
+from python.src.code_238 import Solution
+from python.tests.python_deps.boilerplate import BoilerTest
+
+
+def input_fetcher(case):
+    return case["input"]["nums"]
+
+
+def test():
+    test = BoilerTest(238, input_fetcher)
+    solution = Solution().productExceptSelf
+    test.run_tests(solution)
