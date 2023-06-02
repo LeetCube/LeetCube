611e2b3f465787387748fb8241281025fbcdf80c
Gordon Cheung <gordonhwc1@gmail.com>
2023-06-02 14:12:56 -0700

    change to def test()

--- python/tests/test_22.py
+++ python/tests/test_22.py
@@ -1,12 +1,11 @@
 from python.tests.python_deps.boilerplate import BoilerTest
 from python.src.code_22 import Solution
 
-
 def input_fetcher(case):
     return case["input"]["n"]
 
 
-if __name__ == "__main__":
+def test():
     test = BoilerTest(22, input_fetcher)
     solution = Solution().generateParenthesis
     test.run_tests(solution)
