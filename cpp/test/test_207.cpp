39db9a6cb654b8c4342daec64926abf92374abb7
William Shen <shenpaiwilliam@gmail.com>
2023-06-04 22:42:20 -0700

    Add implementation and tests for Q207

---
+++ cpp/test/test_207.cpp
@@ -0,0 +1,14 @@
+#include "../src/code_207.cpp"
+#include "cpp_deps/boilerplate.hpp"
+
+void test(Solution& sol, const json& input, const json& output) {
+    int numCourses = input["numCourses"].get<int>();
+    auto prerequisites = input["prerequisites"].get<std::vector<std::vector<int>>>();
+    auto expected = output.get<bool>();
+    bool result = sol.canFinish(numCourses, prerequisites);
+    CHECK_EQ(result, expected);
+}
+
+TEST_CASE("") {
+    TEST(207);
+}
