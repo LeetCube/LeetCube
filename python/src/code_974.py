24d809a988b62c8aa9fccd94c68f2c4b2fc46887
ymnglli <ymnglli@gmail.com>
2023-06-08 00:26:29 -0700

    Fixed lint errors

--- python/src/code_974.py
+++ python/src/code_974.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from typing import List
 
+
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
         sums = defaultdict(lambda: 0)
@@ -10,8 +11,9 @@ class Solution:
         currsum = 0
         for n in nums:
             currsum += n
-            if currsum%k in sums:
-               count += sums[currsum%k]
-            sums[currsum%k] += 1
+            if currsum % k in sums:
+               count += sums[currsum % k]
+            sums[currsum % k] += 1
 
         return count
+    
