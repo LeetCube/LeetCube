e7c4840fcea6e2f69b4b45612804c3ca7a9e8dda
Annie Li <86937926+dataprob@users.noreply.github.com>
2023-06-28 23:42:06 -0700

    [Feature] contributors using json (#83)
    * add new contributors
    
    * contributors using json
    
    * minor fix

--- contributors/convert.py
+++ contributors/convert.py
@@ -1,19 +1,28 @@
 import pandas as pd
-from contributors_data import *
+from collections import defaultdict
 
-df_contributors = pd.DataFrame(contributors, columns=columns)
+df_contributors = pd.read_json("../data/contributors.json")
 df_contributors = df_contributors.sort_values("first_commit")
 md_contributors = df_contributors.to_markdown(index=False)
-
 n, _ = df_contributors.shape
 
-df_langs = pd.DataFrame(dict_langs)
+dict_langs = defaultdict(int)
+for langs in df_contributors["langs"]:
+    for lang in langs:
+        dict_langs[lang] += 1
+
+df_langs = pd.DataFrame.from_dict(dict_langs, orient="index", columns=["count"])
+df_langs = df_langs.reset_index(names="langs")
 df_langs = df_langs.sort_values(by="count", ascending=False)
-md_langs = df_langs.to_markdown(index=False)
+ax = df_langs.plot.bar(
+    x="langs", y="count", ylabel="contributors count", rot=0, legend=False
+)
+fig = ax.get_figure()
+fig_name = "figs/langs.png"
+fig.savefig(fig_name)
 
 with open("contributors.md", "w") as file:
     file.write(f"# Contributors\n")
-    file.write(f"We have {n} contributors.\n")
-    file.write(md_langs)
-    file.write(f"\n\n")
+    file.write(f"We have {n} contributors.\n\n")
+    file.write(f"![]({fig_name})\n\n")
     file.write(md_contributors)
