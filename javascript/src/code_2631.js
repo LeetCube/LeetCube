f2ad4c8a25f26be99cfd31b43b9c6ceb7d089398
dataprob <86937926+dataprob@users.noreply.github.com>
2023-07-02 00:18:01 -0700

    format 2631

--- javascript/src/code_2631.js
+++ javascript/src/code_2631.js
@@ -3,13 +3,13 @@
  * @return {Array}
  */
 Array.prototype.groupBy = function (fn) {
-	const dict = {};
-	for (const item of this) {
-		const key = fn(item);
-		key in dict ? dict[key].push(item) : (dict[key] = [item]);
-	}
+  const dict = {};
+  for (const item of this) {
+    const key = fn(item);
+    key in dict ? dict[key].push(item) : (dict[key] = [item]);
+  }
 
-	return dict;
+  return dict;
 };
 
 f = (array, fn) => array.groupBy(fn);
