1d95eb4fa7dafda03e0199a8188be7bd8ca160fa
azalexzhang <azalexzhang13@gmail.com>
2023-07-12 11:53:49 -0700

    Improve solutions to LC 209, 2024 and update contributors list

--- python/src/code_209.py
+++ python/src/code_209.py
@@ -3,24 +3,17 @@ from typing import List
 
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        n = len(nums)
         subArraySum = 0
-        numElements = 0
-        i = 0
-        lowestLength = 100001
+        left = 0
+        lowestLength = n + 1
 
-        for n in nums:
-            subArraySum += n
-            numElements += 1
+        for right, number in enumerate(nums):
+            subArraySum += number
 
             while subArraySum >= target:
-                if numElements < lowestLength:
-                    lowestLength = numElements
+                lowestLength = min(lowestLength, right - left + 1)
+                subArraySum -= nums[left]
+                left += 1
 
-                subArraySum -= nums[i]
-                numElements -= 1
-                i += 1
-
-        if lowestLength == 100001:
-            return 0
-        else:
-            return lowestLength
+        return 0 if lowestLength == n + 1 else lowestLength
