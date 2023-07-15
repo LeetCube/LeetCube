e0ac0eae869fb29b926b9e7a7201dbc3a74e1057
Person314159 <williamzy314@gmail.com>
2023-07-14 23:09:41 -0700

    clang-format boilerplate

--- cpp/test/cpp_deps/boilerplate.hpp
+++ cpp/test/cpp_deps/boilerplate.hpp
@@ -20,22 +20,22 @@ json get_json(const int n) {
 
 void test(Solution& sol, const json& input, const json& output);
 
-#define TEST(n)                                                                                    \
-    {                                                                                              \
-        json tests = get_json(n);                                                                  \
-                                                                                                   \
-        for (json::iterator it = tests.begin(); it != tests.end(); ++it) {                         \
-            const json& test_case = *it;                                                           \
-            const json& input = test_case["input"];                                                \
-            const json& output = test_case["output"];                                              \
+#define TEST(n)                                                                                         \
+    {                                                                                                   \
+        json tests = get_json(n);                                                                       \
+                                                                                                        \
+        for (json::iterator it = tests.begin(); it != tests.end(); ++it) {                              \
+            const json& test_case = *it;                                                                \
+            const json& input = test_case["input"];                                                     \
+            const json& output = test_case["output"];                                                   \
             std::string comments = "\n";                                                                \
             comments += test_case.contains("comments") ? test_case["comments"].get<std::string>() : ""; \
-            Solution sol;                                                                          \
-            INFO("TEST CASE " << (it - tests.begin()));                                            \
-            INFO(comments);                                                                        \
-            /* problem specific */                                                                 \
-            test(sol, input, output);                                                              \
-        }                                                                                          \
+            Solution sol;                                                                               \
+            INFO("TEST CASE " << (it - tests.begin()));                                                 \
+            INFO(comments);                                                                             \
+            /* problem specific */                                                                      \
+            test(sol, input, output);                                                                   \
+        }                                                                                               \
     }
 
 #endif
